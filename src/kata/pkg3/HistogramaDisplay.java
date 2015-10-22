d85f48b354f42b22380742b9464199bb7a95b5cf
usuario <usuario@10.22.143.7>
2015-10-22 10:18:46 +0100

    Versión 2.

--- src/kata/pkg3/HistogramaDisplay.java
+++ src/kata/pkg3/HistogramaDisplay.java
@@ -19,45 +19,42 @@ import org.jfree.ui.ApplicationFrame;
  */
 public class HistogramaDisplay extends ApplicationFrame {
 
-public HistogramaDisplay(){
-super("HISTOGRAMA");
-setContentPane(createPanel());
-pack();
-}
+    private final Histogram<String> histo;
+
+    public HistogramaDisplay(Histogram<String> histo) {
+        super("HISTOGRAMA");
+        this.histo = histo;
+        setContentPane(createPanel());
+        pack();
+    }
 
 
 
-private ChartPanel createPanel(){
-    ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
-    chartPanel.setPreferredSize(new Dimension(500,450));
-    return chartPanel;
 
-}
 
-private JFreeChart createChart (DefaultCategoryDataset dataset){
-    JFreeChart chart = ChartFactory.createBarChart("Histograma de dominios", "DOMINIOS", "Nº Email", dataset, PlotOrientation.VERTICAL, true, true, false);
-    return chart;
-}
+    private ChartPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,450));
+        return chartPanel;
 
-private DefaultCategoryDataset createDataset(){
-    DefaultCategoryDataset dataset =new DefaultCategoryDataset();
-    dataset.addValue(10,"ENE","gmail.com");
-    dataset.addValue(6,"ENE","ulpgc.es");        
-    dataset.addValue(4,"ENE","ull.es");        
-    dataset.addValue(9,"ENE","hotmail.com");    
-    dataset.addValue(8,"FEB","gmail.com");
-    dataset.addValue(4,"FEB","ulpgc.es");        
-    dataset.addValue(2,"FEB","ull.es");        
-    dataset.addValue(10,"FEB","hotmail.com");    
-    dataset.addValue(3,"MAR","gmail.com");
-    dataset.addValue(5,"MAR","ulpgc.es");        
-    dataset.addValue(8,"MAR","ull.es");        
-    dataset.addValue(10,"MAR","hotmail.com");  
-    return dataset;
-}
+    }
 
-public void execute(){
-    setVisible(true);
-}
-}
+    private JFreeChart createChart (DefaultCategoryDataset dataset){
+        JFreeChart chart = ChartFactory.createBarChart("Histograma de dominios", "DOMINIOS", "Nº Email", dataset, PlotOrientation.VERTICAL, true, true, false);
+        return chart;
+    }
+
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataset =new DefaultCategoryDataset();
+        for(String key : histo.keySet()){
+            dataset.addValue(histo.get(key),"",key);
+        }
+        return dataset;
+
+    }
+
+    public void execute(){
+        setVisible(true);
+    }
+    }
 
--- src/kata/pkg3/KATA3.java
+++ src/kata/pkg3/KATA3.java
@@ -5,8 +5,30 @@ public class KATA3 {
 
 
     public static void main(String[] args) {
-
-        HistogramaDisplay histograma = new HistogramaDisplay();
+        Histogram<String> histogram=new Histogram<>();
+        
+        
+        
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        
+        HistogramaDisplay histograma = new HistogramaDisplay(histogram);
         histograma.execute();
        
     }
