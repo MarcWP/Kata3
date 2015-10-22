c8e1a5499954cb47b536853563b96a020fb1d4f1
usuario <usuario@10.22.143.7>
2015-10-22 09:58:04 +0100

    Versión 1.

---
+++ src/kata/pkg3/KATA3.java
@@ -0,0 +1,14 @@
+
+package kata.pkg3;
+
+public class KATA3 {
+
+
+    public static void main(String[] args) {
+
+        HistogramaDisplay histograma = new HistogramaDisplay();
+        histograma.execute();
+       
+    }
+    
+}
